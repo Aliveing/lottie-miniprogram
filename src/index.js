80e062302a582484eddbdbeac7efce11894b5e14
Alive <appreach_ing@Hotmail.com>
2019-12-10 14:53:19 +0800

    upgrade

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import {setup, g} from './adapter'
 const { window, document, navigator } = g
 
-; '__LOTTIE_CANVAS__'
-// ;import '../../node_modules/lottie-web/build/player/lottie_canvas.js'
+// ; '__LOTTIE_CANVAS__'
+;import '../../node_modules/lottie-web/build/player/lottie_canvas.js'
 
 function loadAnimation(options) {
   ['wrapper', 'container'].forEach(key => {
